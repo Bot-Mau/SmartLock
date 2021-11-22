55941af3686efae788dc6e6e1cd73b1845b67f25
Mauricio Aramburu <maramburu98@gmail.com>
2021-11-22 14:20:49 -0600

    Update manageOwner.py

--- Code/manageOwner.py
+++ Code/manageOwner.py
@@ -1,1 +1,37 @@
-
+def changeMasterCode:
+  
+  flag = true
+  
+  while(flag):
+  
+    if(enteredCode == masterCode): # Use the checker class 
+    
+      print("Enter new code: ")
+    
+      newCode = string(input())
+    
+      #Send newCode to OwnerClass to update master passCode
+      
+      flag = false
+     
+     else
+      
+      print("Incorrect code\n")
+    
+def editName:
+  
+  flag = true
+  
+  while(flag):
+  
+    if(enteredCode == masterCode): # Use the checker class 
+    
+      print("Enter new name: ")
+    
+      newName = string(input())
+    
+      #Send newCode to OwnerClass to update master passCode
+      
+      flag = false
+     
+     else
