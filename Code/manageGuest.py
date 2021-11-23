533ff6c1da075e911fb153305800f5708ddafc4b
Mauricio Aramburu <maramburu98@gmail.com>
2021-11-23 17:05:40 -0600

    Updated manageGuest.py

--- Code/manageGuest.py
+++ Code/manageGuest.py
@@ -1,1 +1,101 @@
+def addGuestCode:
+      
+  flag = True
+  
+  print("Adding guest code...\n)
+  
+  while(flag):
+  
+    if(compareMasterCode(enteredCode)): # Use the checker class 
+    
+      print("Enter new guest code: )
+      
+      flag = False
+     
+     else
+      
+      print("Incorrect code...\n")
+  
 
+def changeGuestCode:
+    
+  flag = True
+            
+  print("Changing guest code...\n)
+  
+  while(flag):
+  
+    if(compareMasterCode(enteredCode)): # Use the checker class 
+    
+      print("Enter new guest code: ")
+    
+      newCode = string(input())
+    
+      #Send newCode to OwnerClass to update guest passcode
+      
+      flag = False
+     
+     else
+      
+      print("Incorrect code\n")
+    
+def addGuestName:
+    
+  flag = True
+  
+  while(flag):
+  
+    if(compareMasterCode(enteredCode)): # Use the checker class 
+    
+      print("Enter new guest name: ")
+    
+      newName = string(input())
+    
+      #Send newName to logs to add the guest name
+      
+      flag = False
+     
+     else
+      
+      print("Incorrect code\n")
+  
+def removeGuestName:
+    
+  flag = True
+  
+  while(flag):
+  
+    if(compareMasterCode(enteredCode)): # Use the checker class 
+    
+      print("Enter guest name: ")
+    
+      newName = string(input())
+    
+      #Send newName to logs to remove the guest name
+      
+      flag = False
+     
+     else
+      
+      print("Incorrect code\n")
+      
+def editGuestName:
+      
+  flag = True
+  
+  while(flag):
+  
+    if(compareMasterCode(enteredCode)): # Use the checker class 
+    
+      print("Editing Guest Name\n")
+      
+      removeGuestName()
+      
+      addGuestName()
+      
+      flag = False
+     
+     else
+      
+      print("Incorrect code\n")
+  
