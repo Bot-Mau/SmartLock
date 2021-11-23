20412730c4d98e3bc1c71056ae8783332e41eda9
Mauricio Aramburu <maramburu98@gmail.com>
2021-11-23 17:14:34 -0600

    Update interfaceManagement.py

--- Code/interfaceManagement.py
+++ Code/interfaceManagement.py
@@ -1,1 +1,43 @@
-
+def initializeUI:
+  # All the commands needed to initialize and start the UI for the program 
+  
+def showMasterUI:
+  
+  flag = True
+  
+  count = 0
+  
+  while(flag):
+    
+    if(compareMastercode(enteredCode)):
+      
+      #Display all master UI settings and options
+      
+    else:
+      
+      print("Incorrect passcode...\n")
+      
+      count += 1
+      
+      Error(count)        # Sending count number to the Error class to check for attempts
+      
+def showSettingsUI:
+     
+  flag = True
+  
+  count = 0
+  
+  while(flag):
+    
+    if(compareMastercode(enteredCode)):
+      
+      #Display all settings UI settings and options, only available to master user
+      
+    else:
+      
+      print("Incorrect passcode...\n")
+      
+      count += 1
+      
+      Error(count)        # Sending count number to the Error class to check for attempts
+      
