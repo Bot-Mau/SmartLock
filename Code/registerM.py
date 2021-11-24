a6212710f7cf52ec59d506bcecf986efced7c5fc
KrisTNg <93509240+KrisTNg@users.noreply.github.com>
2021-11-24 16:58:20 -0600

    Add files via upload

---
+++ Code/registerM.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 15:39:27 2021
+
+@author: kris
+"""
+
+
+#this for register the master initially
+def master():
+    mn = (chr(input())) #this for masternames
+    mp = (int(input())) #this for master passcode
+
