dd5f14e3b3dd42116cf4b8ec865e0d9624dc59b5
KrisTNg <93509240+KrisTNg@users.noreply.github.com>
2021-11-24 16:58:36 -0600

    Add files via upload

---
+++ Code/Unlock.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 15:18:16 2021
+
+@author: Kris
+"""
+
+from registerM import*
+
+
+#this for unlock 
+def unlock():
+    pw=int(input())
+    if(pw==mp):
+        unlock()
+    elif(pw==guest_I):
+        unlock()
+    else:
+        Error()
+    
