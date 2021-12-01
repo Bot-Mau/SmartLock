33bb94e75b5df03320d23a894b3e41180eeae6c5
Mauricio Aramburu <maramburu98@gmail.com>
2021-11-30 18:49:37 -0600

    Update manageOwner.py

--- Code/manageOwner.py
+++ Code/manageOwner.py
@@ -1,55 +1,62 @@
-def changeMasterCode:
-  
-  flag = True
-  
-  count = 0
-  
-  print("Changing master passcode...\n")
-  
-  while(flag):
-  
-    if(compareMastercode(enteredCode)): # Use the checker class 
-    
-      print("Enter new code: ")
-    
-      newCode = string(input())
-    
-      #Send newCode to OwnerClass to update master passCode
-      
-      flag = False
-     
-     else:
-      
-      print("Incorrect passcode...\n")
-      
-      count += 1
-      
-      Error(count)        # Sending count number to the Error class to check for attempts
-    
-def editName:
-  
-  flag = True
-  
-  count = 0
-  
-  print("Changing master name...\n")
-  
-  while(flag):
-  
-    if(compareMastercode(enteredCode)): # Use the checker class 
-    
-      print("Enter new name: ")
-    
-      newName = string(input())
-    
-      #Send newCode to OwnerClass to update master passCode
-      
-      flag = False
-     
-     else:
-      
-      print("Incorrect passcode...\n")
-      
-      count += 1
-      
-      Error(count)        # Sending count number to the Error class to check for attempts
+import checker
+import error
+
+
+def changeMasterCode():
+    flag = True
+
+    count = 0
+
+    print("Changing master passcode...\n")
+
+    while flag:
+
+        enteredCode = input("Enter master passcode: ")
+
+        if checker.compareMasterCode(enteredCode):  # Use the checker class
+
+            print("Enter new code: ")
+
+            newCode = input()
+
+            # Send newCode to OwnerClass to update master passCode
+
+            flag = False
+
+        else:
+
+            print("Incorrect passcode...\n")
+
+            count += 1
+
+            error.error(count)  # Sending count number to the Error class to check for attempts
+
+
+def editName():
+    flag = True
+
+    count = 0
+
+    print("Changing master name...\n")
+
+    while flag:
+
+        enteredCode = input("Enter master passcode: ")
+
+        if checker.compareMasterCode(enteredCode):  # Use the checker class
+
+            print("Enter new name: ")
+
+            newName = input()
+
+            # Send newCode to OwnerClass to update master passCode
+
+            flag = False
+
+        else:
+
+            print("Incorrect passcode...\n")
+
+            count += 1
+
+            error.error(count)  # Sending count number to the Error class to check for attempts
