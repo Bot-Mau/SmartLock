f06319a20fb1a9c00192cb92e5e771603d792e26
Mauricio Aramburu <maramburu98@gmail.com>
2021-11-30 18:51:16 -0600

    Update interfaceManagement.py

--- Code/interfaceManagement.py
+++ Code/interfaceManagement.py
@@ -1,43 +1,52 @@
-def initializeUI:
-  # All the commands needed to initialize and start the UI for the program 
-  
-def showMasterUI:
-  
-  flag = True
-  
-  count = 0
-  
-  while(flag):
-    
-    if(compareMastercode(enteredCode)):
-      
-      #Display all master UI settings and options
-      
-    else:
-      
-      print("Incorrect passcode...\n")
-      
-      count += 1
-      
-      Error(count)        # Sending count number to the Error class to check for attempts
-      
-def showSettingsUI:
-     
-  flag = True
-  
-  count = 0
-  
-  while(flag):
-    
-    if(compareMastercode(enteredCode)):
-      
-      #Display all settings UI settings and options, only available to master user
-      
-    else:
-      
-      print("Incorrect passcode...\n")
-      
-      count += 1
-      
-      Error(count)        # Sending count number to the Error class to check for attempts
-      
+import checker
+import error
+
+
+def initializeUI():
+
+
+#All the commands needed to initialize and start the UI for the program 
+
+def showMasterUI():
+    flag = True
+
+    count = 0
+
+    while flag:
+
+        enteredCode = input("Enter master passcode: ")
+
+        if checker.compareMasterCode(enteredCode):
+
+        #Display all master UI settings and options
+
+        else:
+
+            print("Incorrect passcode...\n")
+
+            count += 1
+
+            error.error(count)  # Sending count number to the Error class to check for attempts
+
+
+def showSettingsUI():
+    flag = True
+
+    count = 0
+
+    while flag:
+
+        enteredCode = input("Enter master passcode: ")
+        
+        if checker.compareMasterCode(enteredCode):
+
+        # Display all settings UI settings and options, only available to master user
+
+        else:
+
+            print("Incorrect passcode...\n")
+
+            count += 1
+
+            error.error(count)  # Sending count number to the Error class to check for attempts
+
