e1d603b955d350832f4994faf292def244e3beec
KrisTNg <93509240+KrisTNg@users.noreply.github.com>
2021-12-04 14:02:08 -0600

    Update and rename Error.py to error.py

---
+++ Code/error.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 15:47:20 2021
+
+@author: kris
+"""
+
+#this is for the error
+import alert
+
+def Error(count,password):
+    print("You have input the wrong passcode")  
+ 
+    alert.Alert(count,password)
+    
