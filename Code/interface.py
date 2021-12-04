8436b5511b8ed3207b2012d9e3a6854d60d63d13
KrisTNg <93509240+KrisTNg@users.noreply.github.com>
2021-12-04 14:05:53 -0600

    Update and rename SL interface.py to interface.py

---
+++ Code/interface.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 15:51:13 2021
+
+@author: Kris
+"""
+
+#import checker
+
+#this for the interface 
+def Interface():
+    print("enter passcode below: ")
+    password = int(input())
+    return(password)
+    #checker.Checker(password)
